279da8cb20676790b23cf0293488b60efd425cc2
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-07-26 17:11:18 +0100

    Create RunTransdecoder.sh

---
+++ Step2/RunTransdecoder.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+    TransDecoder.LongOrfs -m 450 -t /home/irh25/jacob/001Genomes/002WholeGenome/WholeTable_18.07.2022.fasta
+    TransDecoder.Predict -t /home/irh25/jacob/001Genomes/002WholeGenome/WholeTable_18.07.2022.fasta
