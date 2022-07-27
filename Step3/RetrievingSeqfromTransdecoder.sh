185cdd3d58e9826a9129268699cbaf2e8729d95c
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-07-27 12:49:50 +0100

    Update and rename ADDSOMETHING.sh to RetrievingSeqfromTransdecoder.sh

---
+++ Step3/RetrievingSeqfromTransdecoder.sh
@@ -0,0 +1,4 @@
+grep ">" longest_orfs.pep > myproteins_fromTransdecoder.txt
+grep ATHILA5 myproteins_fromTransdecoder.txt > myproteins_fromTransdecoder_ATHILA5.txt
+ 
+ #We get a file with the TEs names to be picked up with .R
