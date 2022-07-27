b11d1ce7a1f1cd15f29424078abf3b2be239c0de
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-07-27 14:30:39 +0100

    Update RunBlast.sh

--- Step4/RunBlast.sh
+++ Step4/RunBlast.sh
@@ -1,1 +1,8 @@
+source activate jacob_env
 
+file='Col-0-Weigel.5chrs.fasta_proteins.fa'
+
+cd /home/irh25/jacob/001Genomes/002WholeGenome/001Odd_TEs/Attempt_kewall3_fromFUNCTION/001BLAST_KewAll3
+#makeblastdb -dbtype prot  -in $file  -out ProteinsColDatabaseforBLAST
+
+blastp -query longest_orfs.pep  -db ProteinsColDatabaseforBLAST  -out KewtoColgenes_blast_270722
