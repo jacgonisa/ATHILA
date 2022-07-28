72ade598cbf1b5aa4438345453dfb2e9f8aedde8
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-07-28 12:07:10 +0100

    Update FindRepeatedAfterBLAST.R

--- Step4/FindRepeatedAfterBLAST.R
+++ Step4/FindRepeatedAfterBLAST.R
@@ -3,3 +3,6 @@
     mynonself_athila =  athilaorf_table[athilaorf_table$V1 != athilaorf_table$V2, ]
 
     onlyself = myself_athila[!(myself_athila$V1%in%mynonself_athila$V1),]
+
+## Create a table to visualize in Cytoscape
+    write.table(mynonself_athila[ ,c(1,2)] , "nonself_athilaorf_athila")
