1caea8eca31d481f8dd40976a70269df9d01153a
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-08-01 16:00:07 +0100

    Update RetrievingATHILAORFfromPfamfile.R

--- Step2/Step2.1/RetrievingATHILAORFfromPfamfile.R
+++ Step2/Step2.1/RetrievingATHILAORFfromPfamfile.R
@@ -6,3 +6,13 @@ AllPeptides = read.fasta("Whole ...pep", seqtype="AA")
 ATHILAORF_proteins = AllPeptides[names_ATHILAORF]
 #Get ATHILAX
 ATHILA5_ATHILAORF_proteins = ATHILAORF_proteins[grep("ATHILA5",getName(ATHILAORF_proteins))]
+
+### Modify table: concatenating names
+AllPfam$Concatenated = paste(AllPfam$V19, AllPfam$V20, AllPfam$V21, AllPfam$V22, AllPfam$V23)
+AllPfam$V19 = NULL
+AllPfam$V20 = NULL
+AllPfam$V21 = NULL
+AllPfam$V22 = NULL
+AllPfam$V23 = NULL
+ ###
+
