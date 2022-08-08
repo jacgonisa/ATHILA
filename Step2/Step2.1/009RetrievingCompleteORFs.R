d8410de8ecc5145b19f960ce1a73dc4bc2a6dfb6
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-08-08 17:09:32 +0100

    Update 009RetrievingCompleteORFs.R

--- Step2/Step2.1/009RetrievingCompleteORFs.R
+++ Step2/Step2.1/009RetrievingCompleteORFs.R
@@ -37,3 +37,10 @@ completep3 = unlist(retrievep1(completeORFs))
 completep1p2p3 = c(completep1,completep2,completep3)
 completeORFs = AllPeptides[completep1p2p3]
 completeORFs = Filter(Negate(is.null), completeORFs)
+
+GagPol_Pfam = rbind(AllPfam[AllPfam$Concatenated == "gag-polyprotein putative aspartyl protease ", ], AllPfam[AllPfam$Concatenated == "Aspartyl protease   ", ],AllPfam[AllPfam$Concatenated == "Reverse transcriptase (RNA-dependent DNA polymerase)", ], AllPfam[AllPfam$Concatenated =="Integrase core domain  ", ], AllPfam[AllPfam$Concatenated == "Retrotransposon gag protein  ", ])
+row.names(GagPol_Pfam) = 1:nrow(GagPol_Pfam)
+names_GagPol = GagPol_Pfam$V3
+GagPol_proteins = AllPeptides[names_GagPol]
+GagPol_completeORFs = GagPol_proteins[GagPol_proteins%in%completeORFs]
+write.fasta(getSequence(GagPol_completeORFs), getName(GagPol_completeORFs), file.out= "GagPol_completeORFs080822.fa")
