e006b93316aaa94ee08c7247bb4aef06b668a4c2
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-08-09 16:23:26 +0100

    Update 002RetrievingGagPolORF.R

--- Step3/002RetrievingGagPolORF.R
+++ Step3/002RetrievingGagPolORF.R
@@ -2,6 +2,8 @@ GagPol_Pfam = rbind(AllPfam[AllPfam$Concatenated == "gag-polyprotein putative as
  row.names(GagPol_Pfam) = 1:nrow(GagPol_Pfam)
  names_GagPol = GagPol_Pfam$V3
  GagPol_proteins = AllPeptides[names_GagPol]
+write.table(getSequence(GagPol_proteins), getName(GagPol_proteins), file.out="GagPol_Allproteins_030822.fa")
  ATHILA5_GagPolORF_proteins = GagPol_proteins[grep("ATHILA5",getName(GagPol_proteins))]
+#This last one was called "ATHILA5_GagPolORF_completeandincompleteORFs_proteins_030822.fa"
 ##SEQUENCES ARE REPEATED, USE unique FUNCTION
  
