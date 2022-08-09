f5afdba378b70c93781a08b79793449251e238d9
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-08-09 17:49:15 +0100

    Update RunMAFFT.sh

--- Step8/RunMAFFT.sh
+++ Step8/RunMAFFT.sh
@@ -1,3 +1,4 @@
 source activate jacob_env
-mafft --retree 2 --thread 6  ATHILA5_GagPolORF_completeandincompleteORFs_proteins_030822.fa > test_04082022.phy
-  
+mafft --retree 2 --thread 6  ATHILA5_ATHILAORF_Allproteins_and_OUTGROUPS.fa > AthORF_05082022.fasta
+#Example of how to run MAFFT alignment. Important to include the outgroups!
+
