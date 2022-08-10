7a703423651885bf3162722919af3f83fde5df7d
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-08-10 11:24:20 +0100

    Update and rename Step9/RunIQtree.sh to Step6/RunIQtree.sh

---
+++ Step6/RunIQtree.sh
@@ -0,0 +1,2 @@
+source conda jacob_env
+iqtree -s test_04082022.phy -m MFP -o outgroup1,outgroup2,outgroup3
--- Step9/RunIQtree.sh
+++
@@ -1,2 +0,0 @@
-source conda jacob_env
-iqtree -s test_04082022.phy -m MFP -o Filtered_ATH5_GagPolOutgroupswithBLAST_040822
