eb35a2c8225cdb913e1ec119d87ed834205a8037
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-08-10 09:46:40 +0100

    Update RunMAFFT.sh

--- Step4/RunMAFFT.sh
+++ Step4/RunMAFFT.sh
@@ -1,4 +1,4 @@
 source activate jacob_env
 mafft --retree 2 --thread 6  ATHILA5_ATHILAORF_Allproteins_and_OUTGROUPS.fa > AthORF_05082022.fasta
-#Example of how to run MAFFT alignment. Important to include the outgroups!
+#Example of how to run MAFFT alignment. Important to include the outgroups in the .fasta file!
 
