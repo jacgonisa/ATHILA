6bea2409760a138aebb54931ec338f03ad81f5e6
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-08-15 10:16:38 +0100

    Update 002RetrievingGagPolORF.R

--- Step3/Step3.2/002RetrievingGagPolORF.R
+++ Step3/Step3.2/002RetrievingGagPolORF.R
@@ -2,8 +2,17 @@ GagPol_Pfam = rbind(AllPfam[AllPfam$Concatenated == "gag-polyprotein putative as
  row.names(GagPol_Pfam) = 1:nrow(GagPol_Pfam)
  names_GagPol = GagPol_Pfam$V3
  GagPol_proteins = AllPeptides[names_GagPol]
-write.table(getSequence(GagPol_proteins), getName(GagPol_proteins), file.out="GagPol_Allproteins_030822.fa")
+write.fasta(getSequence(GagPol_proteins), getName(GagPol_proteins), file.out="GagPol_Allproteins_030822.fa")
  ATHILA5_GagPolORF_proteins = GagPol_proteins[grep("ATHILA5",getName(GagPol_proteins))]
 #This last one was called "ATHILA5_GagPolORF_completeandincompleteORFs_proteins_030822.fa"
 ##SEQUENCES ARE REPEATED, USE unique FUNCTION
- 
+
+
+
+#For retrieving only Integrase Pfam
+Integrase_Pfam = AllPfam[AllPfam$Concatenated == "Integrase core domain  ", ]
+row.names(Integrase_Pfam) = 1:nrow(Integrase_Pfam)
+names_Integrase = Integrase_Pfam$V3
+Integrase_proteins = AllPeptides[names_Integrase]
+Integrase_proteins = unique(Integrase_proteins)
+write.fasta(getSequence(Integrase_proteins), getName(Integrase_proteins), file.out="Integrase_Allproteins_030822.fa")
