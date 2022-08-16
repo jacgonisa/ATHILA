99a2927936711a1965841d00f97255109583c817
jacgonisa <109277147+jacgonisa@users.noreply.github.com>
2022-08-16 13:37:46 +0100

    Update 003RetrievingCompleteORFs.R

--- Step3/Step3.3/003RetrievingCompleteORFs.R
+++ Step3/Step3.3/003RetrievingCompleteORFs.R
@@ -46,3 +46,28 @@ GagPol_proteins = AllPeptides[names_GagPol]
 GagPol_completeORFs = GagPol_proteins[GagPol_proteins%in%completeORFs]
 GagPol_completeORFs = unique(GagPol_completeORFs)
 write.fasta(getSequence(GagPol_completeORFs), getName(GagPol_completeORFs), file.out= "GagPol_completeORFs080822.fa")
+
+#GET THE ATHILA-ORFs of each gagpol seq:
+Allathilaorf = read.fasta("ATHILAORF_Allproteins_280722.fa", seqtype="AA")
+completeGagPol = read.fasta("GagPol_completeORFs150822.fa", seqtype="AA")
+names_athila = getName(Allathilaorf)
+names_gagpol = getName(completeGagPol)
+names_gagpol_withoutp1_top2 = gsub(".p1", ".p2", names_gagpol)
+ATHORF_completeGagPol_p1top2= Allathilaorf[names_athila%in%names_gagpol_withoutp1_top2]
+#length = 174
+names_gagpol_withoutp1_top3 = gsub(".p1", ".p3", names_gagpol)
+ATHORF_completeGagPol_p1top3= Allathilaorf[names_athila%in%names_gagpol_withoutp1_top3]
+#length = 41
+names_gagpol_withoutp2_top1 = gsub(".p2", ".p1", names_gagpol)
+ATHORF_completeGagPol_p2top1= Allathilaorf[names_athila%in%names_gagpol_withoutp2_top1]
+#length = 0
+names_gagpol_withoutp2_top3 = gsub(".p2", ".p3", names_gagpol)
+ATHORF_completeGagPol_p2top3= Allathilaorf[names_athila%in%names_gagpol_withoutp2_top3]
+#length = 40
+names_gagpol_withoutp3_top1 = gsub(".p3", ".p1", names_gagpol)
+ATHORF_completeGagPol_p3top1= Allathilaorf[names_athila%in%names_gagpol_withoutp3_top1]
+#length = 0
+
+names_gagpol_withoutp3_top2 = gsub(".p3", ".p2", names_gagpol)
+ATHORF_completeGagPol_p3top2= Allathilaorf[names_athila%in%names_gagpol_withoutp3_top2]
+#length = 21
